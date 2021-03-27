7282f25d410a8305f94af157348a3a71394d3e5f
mooncode163 <chyfemail163@163.com>
2021-03-27 17:07:16 +0800

    ui

--- Apps/Merge/UI/Game/UIBoard.js
+++ Apps/Merge/UI/Game/UIBoard.js
@@ -8,79 +8,14 @@ var UIBoard = cc.Class({
     },
 
     properties: {
-        game: {
-            default: null,
-            type: GameMerge
-        },
-        isShowGame: false,
+  
     },
     onLoad: function () {
-        this._super();
-        this.LoadLanguageGame(); 
-        this.textTitle.node.active = false;
+        this._super(); 
     },
     start: function () {
         this._super();
     },
-    
-
-    CreateGame: function () {
-        var node = cc.instantiate(this.gamePrefab);
-        this.game = node.getComponent(GameShapeColor);
-        this.game.node.parent = this.node;
-        this.game.languageColor = this.languageColor;
-        this.game.cbGameDidError = this.OnGameShapeColorDidError.bind(this);
-        //zorder 让imageBg 显示在最底层，game显示在UI下面
-        this.imageBg.node.zIndex = -20;
-        this.game.node.zIndex = -10;
-        this.isShowGame = true;
-        this.callbackGuankaFinish = null;
-        this.UpdateGuankaLevel(cc.LevelManager.main().gameLevel);
-
-    },
-
-    
-
-    UpdateGuankaLevel: function (level) {
-        cc.Debug.Log("UIGameShapeColor::UpdateGuankaLevel");
-        this._super();
-        this.game.listShape = cc.GameLevelParse.main().listShape;
-        this.game.listColor = cc.GameLevelParse.main().listColor;
-        this.game.textTitle = this.textTitle;
-        this.textTitle.node.active = false;
-
-        this.game.objGameFinish = {
-            onWin: function (ui) {
-                this.OnGameWinFinish(ui, false);
-            }.bind(this),
-            onFail: function (ui) {
-                this.OnGameWinFinish(ui, true);
-            }.bind(this),
-        };
-
-        this.game.LoadGame(cc.GameManager.gameMode);
-      
-
-    },
      
-    
-
-    //interface
-    OnGameWinFinish(ui, isFail) {
-        var info = cc.GameLevelParse.main().GetItemInfo();
-        var strPrefab = ""; 
-
-        // cc.PopUpManager.main().Show({
-        //     prefab: strPrefab,
-        //     open: function (ui) {
-        //         ui.UpdateItem(info);
-        //     }.bind(this),
-        //     close: function (ui) {
-        //     }.bind(this),
-        // });
-
-    },
-
-
 
 });
--- Apps/Merge/UI/Game/UIMergeItem.js
+++ Apps/Merge/UI/Game/UIMergeItem.js
@@ -2,21 +2,56 @@ var UIViewController = require("UIViewController");
 var UIView = require("UIView");
 var UIMergeItem = cc.Class({
     extends: UIView,
-    statics: {
-        
+    statics: { 
     },
 
     properties: { 
+        imageItem: cc.UIImage,  
+        isNew:false,
+        type:0,
     },
     onLoad: function () {
         this._super(); 
+
+        var collider = this.node.getComponent(cc.PhysicsBoxCollider);
+        var ev = this.node.addComponent(cc.UITouchEvent);
+        ev.callBackTouch = this.OnUITouchEvent.bind(this);
     },
     start: function () {
         this._super();
-    },
-    
- 
+    },  
 
+    EnableGravity: function (isEnable) {
+        var bd = this.node.getComponent(cc.RigidBody);
+        // bd.bodyType = isEnable ? RigidbodyType2D.Dynamic : RigidbodyType2D.Static;
+    },
+
+    OnTouchDown: function (pos) {
+    },
+    OnTouchMove: function (pos) {
+    },
+    OnTouchUp: function (pos) {
+    },
+    OnUITouchEvent: function (ev, status, event) {
+
+        var pos = event.getLocation();//canvas坐标原点在屏幕左下角 
+        var posnode = this.node.convertToNodeSpace(pos);//坐标原点在node左下角
+        var posnodeAR = this.node.convertToNodeSpaceAR(pos);//坐标原点在node的锚点
+
+        switch (status) {
+            case cc.UITouchEvent.TOUCH_DOWN:
+                this.OnTouchDown(posnodeAR);
+                break;
+
+            case cc.UITouchEvent.TOUCH_MOVE:
+                this.OnTouchMove(posnodeAR);
+                break;
+
+            case cc.UITouchEvent.TOUCH_UP:
+                this.OnTouchUp(posnodeAR);
+                break;
+        }
+    },
 
 
 });
